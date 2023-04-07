fa19ca2184e04a2179acc7967fad0595448eda31
petegilk <peter_gilkey@yahoo.com>
2023-04-07 01:59:08 -0500

    added mobile responsiveness

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 'use strict'
 
+const width = window.innerWidth;
 const dropdown = document.querySelector('#contact-drop-id');
 const arrow = document.querySelector('#contact-arrow');
+const menuIcon = document.querySelector('#menu-icon');
+const navList = document.querySelector('.nav-list');
 
 arrow.addEventListener('click', () => {
   if (dropdown.classList[0].includes('hide')) {
@@ -13,10 +16,14 @@ arrow.addEventListener('click', () => {
   }
 })
 
-console.log(arrow);
+menuIcon.addEventListener('click', () => {
+  if (menuIcon.getAttribute('name') === 'menu-outline') {
+    navList.style.display = 'block';
+    menuIcon.setAttribute('name', 'close-outline');
+  } else if (menuIcon.getAttribute('name') === 'close-outline') {
+    navList.style.display = 'none';
+    menuIcon.setAttribute('name', 'menu-outline');
+  }
+  
 
-if (dropdown.classList[0].includes('contact-drop')) {
-  console.log('true!')
-}
-
-
+});
